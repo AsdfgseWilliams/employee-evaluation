347bdc18005ed7c36ec6d89e20c9e835d9582d4f
AsdfgseWilliams <ariadna.ballestar.mateo@gmail.com>
2024-11-24 21:28:20 +0100

    modificar función generateToken

--- src/app/api/generate-token/route.ts
+++ src/app/api/generate-token/route.ts
@@ -1,7 +1,6 @@
 import { generateToken } from '@/utils/generateToken';
 
 export async function GET() {
-  // Aquí se genera un token único para el evaluador actual
-  const token = generateToken('evaluador'); // O alguna lógica para identificar al evaluador
+  const token = generateToken(); // O alguna lógica para identificar al evaluador
   return new Response(JSON.stringify({ token }), { status: 200 });
 }
